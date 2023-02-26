7bdd8a7df7d57394c2db3dfd8f975279bebe84ca
sabbir1257 <masabbir1257@gmail.com>
2023-02-26 22:40:36 +0600

    push

--- js/app.js
+++ js/app.js
@@ -1,64 +1,76 @@
-const loadPhone = async (searchText) => {
-    const URL = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`
-    const res = await fetch(URL);
-    const data = await res.json();
-    displayPhone(data.data);
-}
+const loadPhone = async (searchText, dataLimit) => {
+  const URL = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+  const res = await fetch(URL);
+  const data = await res.json();
+  displayPhone(data.data, dataLimit);
+};
 
-const displayPhone = phones => {
-    const phoneContainer = document.getElementById('phones-container');
-    phoneContainer.textContent = '';
-    // display 20 phones only
-    phones = phones.slice(0, 20);
-
-    // display no phone found
-    const noPhone = document.getElementById('no-found-message');
-    if(phones.length === 0){
-        noPhone.classList.remove('d-none');
-    }
-    else{
-        noPhone.classList.add('d-none')
-    }
-    // display all phons
-    phones.forEach(phone => {
-        const phoneDiv = document.createElement('div');
-        phoneDiv.classList.add('col');
-        phoneDiv.innerHTML = `
+const displayPhone = (phones, dataLimit) => {
+  const phoneContainer = document.getElementById("phones-container");
+  phoneContainer.textContent = '';
+  // display 20 phones only
+  const showAll = document.getElementById("show-all");
+  if (dataLimit && phones.length > 10) {
+    phones = phones.slice(0, 10);
+    showAll.classList.remove("d-none");
+  } else {
+    showAll.classList.add("d-none");
+  }
+  
+  const noPhone = document.getElementById("no-found-message");
+  // display no phone found
+  if (phones.length === 0) {
+    noPhone.classList.remove("d-none");
+  } else {
+    noPhone.classList.add("d-none");
+  }
+  // display all phons
+  phones.forEach((phone) => {
+    const phoneDiv = document.createElement("div");
+    phoneDiv.classList.add("col");
+    phoneDiv.innerHTML = `
         <div class="card p-4">
+        longer.
               <img src="${phone.image}" class="card-img-top" alt="..." />
               <div class="card-body">
                 <h5 class="card-title">${phone.phone_name} </h5>
                 <p class="card-text">
                   This is a longer card with supporting text below as a natural
                   lead-in to additional content. This content is a little bit
-                  longer.
                 </p>
               </div>
             </div>
         `;
-        phoneContainer.appendChild(phoneDiv);
-    })
-    // stop spinner or loader
-    toggleSpinner(false);
+    phoneContainer.appendChild(phoneDiv);
+  });
+  // stop spinner or loader
+  toggleSpinner(false);
+};
+
+const procrssSearch = (dataLimit) => {
+    toggleSpinner(true);
+    const searchField = document.getElementById("search-field");
+    const searchText = searchField.value;
+    loadPhone(searchText, dataLimit);
 }
 
 // handle search button click
-document.getElementById('btn-search').addEventListener('click', function(){
-    // start loader
-    toggleSpinner(true);
-    const searchField = document.getElementById('search-field');
-    const searchText = searchField.value;
-    loadPhone(searchText);
+document.getElementById("btn-search").addEventListener("click", function () {
+  // start loader
+  procrssSearch(10);
 });
 
-const toggleSpinner = isLoading => {
-    const loaderSection = document.getElementById('loader');
-    if(isLoading){
-        loaderSection.classList.remove('d-none')
-    }
-    else{
-        loaderSection.classList.add('d-none')
-    }
-}
+const toggleSpinner = (isLoading) => {
+  const loaderSection = document.getElementById("loader");
+  if (isLoading) {
+    loaderSection.classList.remove("d-none");
+  } else {
+    loaderSection.classList.add("d-none");
+  }
+};
+
+document.getElementById('btn-show-all').addEventListener('click', function(){
+    procrssSearch();
+})
 
 // loadPhone();
